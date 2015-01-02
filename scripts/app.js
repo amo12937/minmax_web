7a181e555fc2516b5b7977c362782d3fc7b5cbfc
s_anno <amo.20325@gmail.com>
2015-01-02 19:33:26 +0900

    不要なファイル削除

--- scripts/app.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  "use strict";
-  (function(modulePrefix) {
-    return angular.module("" + modulePrefix + ".Main", ["ng", "ngLoadScript", "" + modulePrefix + ".BoardMaster", "" + modulePrefix + ".Player", "" + modulePrefix + ".controllers"]);
-  })("amo.minmax");
-
-}).call(this);
-
-//# sourceMappingURL=app.js.map
